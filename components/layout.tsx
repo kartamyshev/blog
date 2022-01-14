15d55da5efcef32b88ced21843fbd5d0518029ed
kartamyshev <kartamyshev.k@yandex.ru>
2022-01-14 12:11:24 +0100

    styling posts list

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,19 +1,16 @@
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
 import { useRouter } from "next/router";
+import styles from "./layout.module.css";
+import Header from "./header";
 
-const name = "John Doe";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({
   children,
-  home,
+  // home,
 }: {
   children: React.ReactNode;
-  home?: boolean;
+  // home?: boolean;
 }) {
   const { back } = useRouter();
 
@@ -34,47 +31,13 @@ export default function Layout({
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpeg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpeg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
+      <Header />
       <main>{children}</main>
-      {!home && (
+      {/* {!home && (
         <div className={styles.backToHome}>
           <a onClick={back}>← Back</a>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -4,13 +4,14 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../../components/layout";
 import utilStyles from "../../styles/utils.module.css";
 import posts from "../../lib/posts";
+import styles from './posts.module.scss';
 
 import { formatDate } from "../../utils/formatDate";
 
 export const getStaticProps: GetStaticProps = () => {
   return {
     props: {
-      posts: posts.getAll(),
+      posts: posts.getAllPublished(),
     },
   };
 };
@@ -27,28 +28,22 @@ export default function Posts({ posts }: { posts: Array<Post> }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-        <ul className={utilStyles.list}>
-          {posts.map((post) => {
-            return (
-              <li className={utilStyles.listItem} key={post.id}>
+      <ul className={styles.postList}>
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              <span className={styles.postMeta}>
+                {formatDate(post.date)}
+              </span>
+              <h2 className={styles.postLink}>
                 <Link href={`/posts/${post.id}`}>
                   <a>{post.title}</a>
                 </Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  {formatDate(post.date)}
-                </small>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-    </Layout>
+              </h2>
+            </li>
+          );
+        })}
+      </ul>
+  </Layout>
   );
 }
